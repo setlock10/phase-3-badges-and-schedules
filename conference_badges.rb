1767c7e929d5a90854115287a248981cba7ed546
John J Setlock II <johnsetlock@gmail.com>
2022-08-29 08:47:53 -0500

    Completed Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
+require 'pry'
+
+
 # Write your code here.
+
+def attendees
+    ["steve","sally","jane"]
+end
+
+
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges=attendees.map do |name|
+        badge_maker(name)
+        
+    end
+end
+
+def assign_rooms(attendees)
+    roomlist=[]
+    attendees.each_with_index {|name,index|
+    roomlist<<"Hello, #{name}! You'll be assigned to room #{index+1}!"
+    puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
+}
+    return roomlist
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees)
+    assign_rooms(attendees)
+end
+
+binding.pry
